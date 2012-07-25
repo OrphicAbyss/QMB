678f1c0b1422b5dc13ec9cd36aaf76d28e3b1071
Chris Hallson <drlabman@gluonporridge.net>
2012-07-26 07:41:57 +1000

    Minor cleanup.

--- wad.h
+++ wad.h
@@ -29,7 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define	TYP_MIPTEX		68
 
 typedef struct {
-	int			width
+	int			width;
 	int			height;
 	byte		data[4];			// variably sized
 } qpic_t;
